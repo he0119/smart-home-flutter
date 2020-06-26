59864ad12cf89a4b98bd1b37da3fc7ddb943af7d
hemengyang <hmy0119@gmail.com>
2020-06-26 11:12:09 +0800

    修复检查更新时判断平台的错误
    web 不支持 Platform

--- lib/blocs/update/update_bloc.dart
+++ lib/blocs/update/update_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:io' show Platform;
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 import 'package:smart_home/repositories/version_repository.dart';
 import 'package:version/version.dart';
@@ -17,7 +18,7 @@ class UpdateBloc extends Bloc<UpdateEvent, UpdateState> {
     UpdateEvent event,
   ) async* {
     // 暂时只支持 Android
-    if (event is UpdateStarted && Platform.isAndroid) {
+    if (event is UpdateStarted && !kIsWeb && Platform.isAndroid) {
       bool needUpdate = await versionRepository.needUpdate();
       if (needUpdate) {
         String url = await versionRepository.updateUrl();
