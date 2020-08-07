b7f0443dd2814e1d2d84da51f6ce8d85880b83dd
hemengyang <hmy0119@gmail.com>
2020-08-07 20:53:28 +0800

    增加设置界面 (#61)
    * 添加 Drawer
    
    * 增加关于对话框
    
    * 添加一个统一的设置页面 close #59
    
    * 更新 flutter 编译版本

---
+++ lib/pages/settings/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:smart_home/blocs/app_preferences/app_preferences_bloc.dart';
+import 'package:smart_home/pages/settings/blog/blog_admin_url_page.dart';
+import 'package:smart_home/pages/settings/blog/blog_url_page.dart';
+import 'package:smart_home/pages/settings/common/api_url_page.dart';
+import 'package:smart_home/pages/settings/iot/refresh_interval_page.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('设置'),
+      ),
+      body: BlocBuilder<AppPreferencesBloc, AppPreferencesState>(
+        builder: (context, state) => SettingsList(
+          sections: [
+            SettingsSection(
+              title: '通用',
+              tiles: [
+                SettingsTile(
+                  title: '服务器网址',
+                  subtitle: state.apiUrl ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => ApiUrlPage(),
+                    ));
+                  },
+                ),
+                SettingsTile(
+                  title: '默认主页',
+                  subtitle: '物品',
+                  onTap: () {},
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: 'IOT',
+              tiles: [
+                SettingsTile(
+                  title: '刷新间隔',
+                  subtitle: state.refreshInterval.toString() ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => RefreshIntervalPage(),
+                    ));
+                  },
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: '博客',
+              tiles: [
+                SettingsTile(
+                  title: '博客网址',
+                  subtitle: state.blogUrl ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => BlogUrlPage(),
+                    ));
+                  },
+                ),
+                SettingsTile(
+                  title: '博客管理网址',
+                  subtitle: state.blogAdminUrl ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => BlogAdminUrlPage(),
+                    ));
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smart_home/blocs/authentication/authentication_bloc.dart';
+import 'package:smart_home/pages/settings/home_page.dart';
+import 'package:smart_home/repositories/repositories.dart';
+import 'package:smart_home/widgets/gravatar.dart';
+import 'package:version/version.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Drawer(
+        child: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: (context, state) {
+            if (state is AuthenticationSuccess) {
+              return ListView(
+                children: [
+                  UserAccountsDrawerHeader(
+                    accountName: Text(state.currentUser.username),
+                    accountEmail: Text(state.currentUser.email),
+                    currentAccountPicture:
+                        CircleGravatar(email: state.currentUser.email),
+                  ),
+                  ListTile(
+                    title: Text('设置'),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => SettingsPage(),
+                      ));
+                    },
+                  ),
+                  ListTile(
+                    title: Text('关于'),
+                    onTap: () async {
+                      Version currentVersion =
+                          await RepositoryProvider.of<VersionRepository>(
+                                  context)
+                              .currentVersion;
+                      showAboutDialog(
+                        context: context,
+                        applicationVersion: currentVersion.toString(),
+                        applicationIcon: ImageIcon(
+                          AssetImage('assets/icon/icon.png'),
+                          color: Color(0xFFF15713),
+                        ),
+                        applicationLegalese: '智慧家庭的客户端',
+                      );
+                    },
+                  ),
+                ],
+              );
+            }
+            return Container();
+          },
+        ),
+      ),
+    );
+  }
+}
