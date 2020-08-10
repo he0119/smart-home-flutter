3ab43f6a2e9e2570744994d633c48ab1599b7264
hemengyang <hmy0119@gmail.com>
2020-08-10 10:31:30 +0800

    调整登录页面的显示

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -166,64 +166,60 @@ class _LoginFormState extends State<LoginForm> {
       );
     }
 
-    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
+    return BlocConsumer<AuthenticationBloc, AuthenticationState>(
+      listener: (context, state) {
+        if (state is AuthenticationInProgress) {
+          showInfoSnackBar(context, '正在登录...', duration: 1);
+        }
+      },
       builder: (context, state) {
-        return Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/icon/icon.png',
-                  width: 100.0,
-                  height: 100.0,
-                  semanticLabel: 'icon',
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Form(
-                    child: AutofillGroup(
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            enableSuggestions: false,
-                            decoration: InputDecoration(labelText: '用户名'),
-                            controller: _usernameController,
-                            autofillHints: <String>[AutofillHints.username],
-                          ),
-                          TextFormField(
-                            enableSuggestions: false,
-                            decoration: InputDecoration(labelText: '密码'),
-                            controller: _passwordController,
-                            obscureText: true,
-                            autofillHints: <String>[AutofillHints.password],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                  ),
-                  onPressed: state is! AuthenticationInProgress
-                      ? _onLoginButtonPressed
-                      : null,
-                  child: Text('登录'),
-                ),
-                FlatButton(
-                  onPressed: widget.onTapBack,
-                  child: Text('返回'),
-                ),
-                Container(
-                  child: state is AuthenticationInProgress
-                      ? CircularProgressIndicator()
-                      : null,
-                ),
-              ],
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon/icon.png',
+              width: 100.0,
+              height: 100.0,
+              semanticLabel: 'icon',
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                child: AutofillGroup(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        enableSuggestions: false,
+                        decoration: InputDecoration(labelText: '用户名'),
+                        controller: _usernameController,
+                        autofillHints: <String>[AutofillHints.username],
+                      ),
+                      TextFormField(
+                        enableSuggestions: false,
+                        decoration: InputDecoration(labelText: '密码'),
+                        controller: _passwordController,
+                        obscureText: true,
+                        autofillHints: <String>[AutofillHints.password],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              ),
+              onPressed: state is! AuthenticationInProgress
+                  ? _onLoginButtonPressed
+                  : null,
+              child: Text('登录'),
+            ),
+            FlatButton(
+              onPressed: widget.onTapBack,
+              child: Text('返回'),
+            ),
+          ],
         );
       },
     );
