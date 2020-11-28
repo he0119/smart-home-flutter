680beed805cac014c18a838f53d87f065b4eb027
hemengyang <hmy0119@gmail.com>
2020-11-28 13:12:59 +0800

    可在设置界面查看小米推送的注册状态

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:smart_home/blocs/app_preferences/app_preferences_bloc.dart';
+import 'package:smart_home/blocs/push/push_bloc.dart';
 import 'package:smart_home/models/app_tab.dart';
 import 'package:smart_home/pages/settings/blog/blog_admin_url_page.dart';
 import 'package:smart_home/pages/settings/blog/blog_url_page.dart';
@@ -18,71 +19,91 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('设置'),
       ),
-      body: BlocBuilder<AppPreferencesBloc, AppPreferencesState>(
-        builder: (context, state) => SettingsList(
-          sections: [
-            SettingsSection(
-              title: '通用',
-              tiles: [
-                SettingsTile(
-                  title: '服务器网址',
-                  subtitle: state.apiUrl ?? '请单击输入',
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => ApiUrlPage(),
-                    ));
-                  },
+      body: BlocBuilder<PushBloc, PushState>(
+        builder: (context, state) {
+          String _pushStatus = '未注册（单击重试）';
+          if (state is PushInProgress) {
+            _pushStatus = '注册中（单击重试）';
+          }
+          if (state is PushSuccess) {
+            _pushStatus = '已注册';
+          }
+          return BlocBuilder<AppPreferencesBloc, AppPreferencesState>(
+            builder: (context, state) => SettingsList(
+              sections: [
+                SettingsSection(
+                  title: '通用',
+                  tiles: [
+                    SettingsTile(
+                      title: '服务器网址',
+                      subtitle: state.apiUrl ?? '请单击输入',
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ApiUrlPage(),
+                        ));
+                      },
+                    ),
+                    SettingsTile(
+                      title: '默认主页',
+                      subtitle: state.defaultPage.name,
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => DefaultPage(),
+                        ));
+                      },
+                    ),
+                    SettingsTile(
+                      title: '小米推送',
+                      subtitle: _pushStatus,
+                      onTap: () {
+                        if (_pushStatus != '已注册') {
+                          BlocProvider.of<PushBloc>(context).add(PushStarted());
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                SettingsTile(
-                  title: '默认主页',
-                  subtitle: state.defaultPage.name,
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => DefaultPage(),
-                    ));
-                  },
+                SettingsSection(
+                  title: '物联网',
+                  tiles: [
+                    SettingsTile(
+                      title: '刷新间隔',
+                      subtitle: state.refreshInterval.toString() ?? '请单击输入',
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => RefreshIntervalPage(),
+                        ));
+                      },
+                    ),
+                  ],
+                ),
+                SettingsSection(
+                  title: '博客',
+                  tiles: [
+                    SettingsTile(
+                      title: '博客网址',
+                      subtitle: state.blogUrl ?? '请单击输入',
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => BlogUrlPage(),
+                        ));
+                      },
+                    ),
+                    SettingsTile(
+                      title: '博客管理网址',
+                      subtitle: state.blogAdminUrl ?? '请单击输入',
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => BlogAdminUrlPage(),
+                        ));
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
-            SettingsSection(
-              title: '物联网',
-              tiles: [
-                SettingsTile(
-                  title: '刷新间隔',
-                  subtitle: state.refreshInterval.toString() ?? '请单击输入',
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => RefreshIntervalPage(),
-                    ));
-                  },
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: '博客',
-              tiles: [
-                SettingsTile(
-                  title: '博客网址',
-                  subtitle: state.blogUrl ?? '请单击输入',
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => BlogUrlPage(),
-                    ));
-                  },
-                ),
-                SettingsTile(
-                  title: '博客管理网址',
-                  subtitle: state.blogAdminUrl ?? '请单击输入',
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => BlogAdminUrlPage(),
-                    ));
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
