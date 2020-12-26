90dd33ef01103434047868d601dbd6df82ac62dd
hemengyang <hmy0119@gmail.com>
2020-12-26 16:43:27 +0800

    仅在安卓上显示小米推送的设置

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:settings_ui/settings_ui.dart';
@@ -8,6 +11,7 @@ import 'package:smart_home/pages/settings/board/comment_order_page.dart';
 import 'package:smart_home/pages/settings/common/api_url_page.dart';
 import 'package:smart_home/pages/settings/common/default_page.dart';
 import 'package:smart_home/pages/settings/common/mipush_page.dart';
+import 'package:smart_home/pages/settings/common/mipush_settings_tile.dart';
 import 'package:smart_home/pages/settings/iot/refresh_interval_page.dart';
 import 'package:smart_home/models/app_tab.dart';
 
@@ -20,105 +24,86 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('设置'),
       ),
-      body: BlocBuilder<PushBloc, PushState>(
-        builder: (context, state) {
-          String _pushStatus = '未注册';
-          if (state is PushInProgress) {
-            _pushStatus = '注册中';
-          }
-          if (state is PushSuccess) {
-            _pushStatus = '已注册';
-          }
-          return BlocBuilder<AppPreferencesBloc, AppPreferencesState>(
-            builder: (context, state) => SettingsList(
-              sections: [
-                SettingsSection(
-                  title: '通用',
-                  tiles: [
-                    SettingsTile(
-                      title: '服务器网址',
-                      subtitle: state.apiUrl ?? '请单击输入',
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => ApiUrlPage(),
-                        ));
-                      },
-                    ),
-                    SettingsTile(
-                      title: '默认主页',
-                      subtitle: state.defaultPage.name,
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => DefaultPage(),
-                        ));
-                      },
-                    ),
-                    SettingsTile(
-                      title: '小米推送',
-                      subtitle: _pushStatus,
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => MiPushPage(),
-                        ));
-                      },
-                    ),
-                  ],
+      body: BlocBuilder<AppPreferencesBloc, AppPreferencesState>(
+        builder: (context, state) => SettingsList(
+          sections: [
+            SettingsSection(
+              title: '通用',
+              tiles: [
+                SettingsTile(
+                  title: '服务器网址',
+                  subtitle: state.apiUrl ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => ApiUrlPage(),
+                    ));
+                  },
                 ),
-                SettingsSection(
-                  title: '物联网',
-                  tiles: [
-                    SettingsTile(
-                      title: '刷新间隔',
-                      subtitle: state.refreshInterval.toString() ?? '请单击输入',
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => RefreshIntervalPage(),
-                        ));
-                      },
-                    ),
-                  ],
+                SettingsTile(
+                  title: '默认主页',
+                  subtitle: state.defaultPage.name,
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => DefaultPage(),
+                    ));
+                  },
                 ),
-                SettingsSection(
-                  title: '博客',
-                  tiles: [
-                    SettingsTile(
-                      title: '博客网址',
-                      subtitle: state.blogUrl ?? '请单击输入',
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => BlogUrlPage(),
-                        ));
-                      },
-                    ),
-                    SettingsTile(
-                      title: '博客管理网址',
-                      subtitle: state.blogAdminUrl ?? '请单击输入',
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => BlogAdminUrlPage(),
-                        ));
-                      },
-                    ),
-                  ],
-                ),
-                SettingsSection(
-                  title: '留言板',
-                  tiles: [
-                    SettingsTile(
-                      title: '评论排序',
-                      subtitle: state.commentDescending ? '倒序' : '正序',
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => CommentOrderPage(),
-                        ));
-                      },
-                    ),
-                  ],
+                if (!kIsWeb && Platform.isAndroid) MiPushSettingsTile(),
+              ],
+            ),
+            SettingsSection(
+              title: '物联网',
+              tiles: [
+                SettingsTile(
+                  title: '刷新间隔',
+                  subtitle: state.refreshInterval.toString() ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => RefreshIntervalPage(),
+                    ));
+                  },
                 ),
               ],
             ),
-          );
-        },
+            SettingsSection(
+              title: '博客',
+              tiles: [
+                SettingsTile(
+                  title: '博客网址',
+                  subtitle: state.blogUrl ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => BlogUrlPage(),
+                    ));
+                  },
+                ),
+                SettingsTile(
+                  title: '博客管理网址',
+                  subtitle: state.blogAdminUrl ?? '请单击输入',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => BlogAdminUrlPage(),
+                    ));
+                  },
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: '留言板',
+              tiles: [
+                SettingsTile(
+                  title: '评论排序',
+                  subtitle: state.commentDescending ? '倒序' : '正序',
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => CommentOrderPage(),
+                    ));
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
