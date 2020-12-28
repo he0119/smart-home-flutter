0739f911a0a2930c8f7db357c0d8af5533847280
hemengyang <hmy0119@gmail.com>
2020-12-29 00:33:59 +0800

    添加支持 sliver 的无限列表

--- lib/widgets/infinite_list.dart
+++ lib/widgets/infinite_list.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:smart_home/widgets/bottom_loader.dart';
 
 /// 无限长列表
+///
 /// 随着用户的滚动载入新数据
+///
+/// 使用的 [ListView.separated], 分隔为 [Divider]
 class InfiniteList<T> extends StatefulWidget {
   final List<T> items;
   final Widget Function(BuildContext context, T item) itemBuilder;
@@ -44,6 +47,67 @@ class _InfiniteListState<T> extends State<InfiniteList<T>> {
         },
         separatorBuilder: (contexit, index) => Divider(),
         controller: _scrollController,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= widget.threshold &&
+        !widget.hasReachedMax) {
+      if (widget.onFetch != null) widget.onFetch();
+    }
+  }
+}
+
+class SliverInfiniteList<T> extends StatefulWidget {
+  final List<Widget> slivers;
+  final bool hasReachedMax;
+  final VoidCallback onFetch;
+  final double threshold;
+
+  const SliverInfiniteList({
+    Key key,
+    this.slivers,
+    this.onFetch,
+    this.hasReachedMax = true,
+    this.threshold = 200,
+  })  : assert(hasReachedMax != null),
+        super(key: key);
+
+  @override
+  _SliverInfiniteListState createState() => _SliverInfiniteListState<T>();
+}
+
+class _SliverInfiniteListState<T> extends State<SliverInfiniteList<T>> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      controller: _scrollController,
+      child: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          ...widget.slivers,
+          if (!widget.hasReachedMax)
+            SliverToBoxAdapter(
+              child: BottomLoader(),
+            ),
+        ],
       ),
     );
   }
