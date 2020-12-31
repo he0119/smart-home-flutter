32b2abbdafcddcd2e2a35e6bc4a9ffe6c7e5d566
hemengyang <hmy0119@gmail.com>
2020-12-31 17:15:51 +0800

    尝试移除网址上的 # 号

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:smart_home/app_config.dart';
 
 import 'main.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   var configuredApp = AppConfig(
     appName: '智慧家庭',
     flavorName: 'prod',
