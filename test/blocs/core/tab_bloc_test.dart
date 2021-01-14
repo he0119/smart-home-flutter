a84a7575e3b9bc22da6b4c2dc68eee9efa0f7bf1
hemengyang <hmy0119@gmail.com>
2021-01-14 10:47:32 +0800

    添加 tab_bloc 的测试

---
+++ test/blocs/core/tab_bloc_test.dart
@@ -0,0 +1,32 @@
+// Copyright 2018 The Flutter Architecture Sample Authors. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_home/blocs/core/tab/tab_bloc.dart';
+import 'package:smart_home/models/models.dart';
+
+void main() {
+  group('TabBloc', () {
+    // ignore: close_sinks
+    TabBloc tabBloc;
+
+    setUp(() {
+      tabBloc = TabBloc();
+    });
+
+    test('initial state is null', () {
+      expect(tabBloc.state, null);
+    });
+
+    blocTest<TabBloc, AppTab>(
+      'should update the AppTab',
+      build: () => tabBloc,
+      act: (TabBloc bloc) async => bloc.add(TabChanged(AppTab.board)),
+      expect: <AppTab>[
+        AppTab.board,
+      ],
+    );
+  });
+}
