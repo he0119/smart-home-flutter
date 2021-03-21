3daf767bdff4697097f9738b1759ec60a345da62
hemengyang <hmy0119@gmail.com>
2021-03-21 10:50:18 +0800

    调整客户端代码

--- lib/repositories/graphql_api_client.dart
+++ lib/repositories/graphql_api_client.dart
@@ -77,38 +77,39 @@ class GraphQLApiClient {
 
   /// 初始化 GraphQL 客户端
   Future<bool> initailize(String url) async {
-    final AuthLink _authLink =
-        AuthLink(getToken: () async => 'JWT ${await token}');
-    // 用户代理设置为当前手机
-    // 暂时只支持 Android
-    // SmartHome/0.6.1 (Linux; Android 10; Mi-4c Build/QQ3A.200805.001)
-    Map<String, String> headers = {};
-    if (!kIsWeb && Platform.isAndroid) {
-      final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-      final PackageInfo packageInfo = await PackageInfo.fromPlatform();
-      headers['User-Agent'] =
-          'SmartHome/${packageInfo.version} (Linux; Android ${androidInfo.version.release}; ${androidInfo.model} Build/${androidInfo.id})';
-    }
-    final HttpLink _httpLink = HttpLink(
-      url,
-      defaultHeaders: headers,
-    );
-    final ErrorLink _tokenErrorLink =
-        ErrorLink(onGraphQLError: _handleTokenError);
-    final Link _link = _tokenErrorLink.split((request) {
-      final DefinitionNode definition =
-          request.operation.document.definitions.first;
-      if (definition.runtimeType == OperationDefinitionNode) {
-        String operationName = "";
-        operationName = (definition as OperationDefinitionNode).name!.value;
-        if (operationName == 'tokenAuth' || operationName == 'refreshToken') {
-          return false;
-        }
-      }
-      return true;
-    }, _authLink.concat(_httpLink), _httpLink);
     try {
+      final AuthLink _authLink =
+          AuthLink(getToken: () async => 'JWT ${await token}');
+      // 用户代理设置为当前手机
+      // 暂时只支持 Android
+      // SmartHome/0.6.1 (Linux; Android 10; Mi-4c Build/QQ3A.200805.001)
+      Map<String, String> headers = {};
+      if (!kIsWeb && Platform.isAndroid) {
+        final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+        final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+        headers['User-Agent'] =
+            'SmartHome/${packageInfo.version} (Linux; Android ${androidInfo.version.release}; ${androidInfo.model} Build/${androidInfo.id})';
+      }
+      final HttpLink _httpLink = HttpLink(
+        url,
+        defaultHeaders: headers,
+      );
+      final ErrorLink _tokenErrorLink =
+          ErrorLink(onGraphQLError: _handleTokenError);
+      final Link _link = _tokenErrorLink.split((request) {
+        final DefinitionNode definition =
+            request.operation.document.definitions.first;
+        if (definition.runtimeType == OperationDefinitionNode) {
+          final String operationName =
+              (definition as OperationDefinitionNode).name!.value;
+          if (operationName == 'tokenAuth' || operationName == 'refreshToken') {
+            return false;
+          }
+        }
+        return true;
+      }, _authLink.concat(_httpLink), _httpLink);
+
       _client = GraphQLClient(
         cache: GraphQLCache(),
         link: _link,
@@ -167,7 +168,7 @@ class GraphQLApiClient {
   /// 处理令牌相关的问题
   Stream<Response> _handleTokenError(
       Request request, forward, Response response) async* {
-    String message = response.errors![0].message.toLowerCase();
+    String message = response.errors!.first.message.toLowerCase();
     if ([
       'signature has expired',
       'error decoding signature',
@@ -204,7 +205,8 @@ class GraphQLApiClient {
     QueryResult results = await _client!.mutate(options);
 
     // 如果刷新令牌出错，则返回错误
-    if (results.hasException && results.exception!.graphqlErrors.isNotEmpty) {
+    final exception = results.exception;
+    if (exception != null && exception.graphqlErrors.isNotEmpty) {
       return results.exception!.graphqlErrors;
     } else {
       String token = results.data!['refreshToken']['token'];
