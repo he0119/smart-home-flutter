abeaf800a573f14a44175cb45be66e938bedaaff
hemengyang <hmy0119@gmail.com>
2021-03-27 17:35:52 +0800

    添加 sentry
    直接把 dsn 写在应用中

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,11 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:smarthome/app/app_config.dart';
 
 import 'package:smarthome/app/configure_nonweb.dart'
     if (dart.library.html) 'package:smarthome/app/configure_web.dart';
 import 'package:smarthome/app/main.dart';
 
-void main() {
+Future<void> main() async {
   configureApp();
   var configuredApp = AppConfig(
     appName: '智慧家庭',
@@ -13,5 +16,17 @@ void main() {
     apiUrl: 'https://smart.hehome.xyz/graphql',
     child: MyApp(),
   );
-  runApp(configuredApp);
+  runZonedGuarded(() async {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn =
+            'https://dcc18c1b89d44e1894c4c712ea166c84@o480939.ingest.sentry.io/5685513';
+        options.environment = 'prod';
+      },
+    );
+
+    runApp(configuredApp);
+  }, (exception, stackTrace) async {
+    await Sentry.captureException(exception, stackTrace: stackTrace);
+  });
 }
