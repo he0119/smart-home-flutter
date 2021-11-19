80067593bafd18540e755df633bb044ebd82fca8
hemengyang <hmy0119@gmail.com>
2021-11-20 00:31:20 +0800

    将所有配置都转移到这个位置

--- lib/core/settings/settings_controller.dart
+++ lib/core/settings/settings_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smarthome/core/model/app_tab.dart';
+import 'package:smarthome/user/model/user.dart';
 
 import 'settings_service.dart';
 
@@ -17,16 +19,48 @@ class SettingsController with ChangeNotifier {
   // Make ThemeMode a private variable so it is not updated directly without
   // also persisting the changes with the SettingsService.
   late ThemeMode _themeMode;
+  late User? _loginUser;
+  late String? _apiUrl;
+  late String? _miPushAppId;
+  late String? _miPushAppKey;
+  late String? _miPushRegId;
+  late int _refreshInterval;
+  late String? _adminUrl;
+  late String _blogUrl;
+  late String? _blogAdminUrl;
+  late AppTab _defaultPage;
+  late bool _commentDescending;
 
   // Allow Widgets to read the user's preferred ThemeMode.
   ThemeMode get themeMode => _themeMode;
+  User? get loginUser => _loginUser;
+  String? get apiUrl => _apiUrl;
+  String? get miPushAppId => _miPushAppId;
+  String? get miPushAppKey => _miPushAppKey;
+  String? get miPushRegId => _miPushRegId;
+  int get refreshInterval => _refreshInterval;
+  String? get adminUrl => _adminUrl;
+  String get blogUrl => _blogUrl;
+  String? get blogAdminUrl => _blogAdminUrl;
+  AppTab get defaultPage => _defaultPage;
+  bool get commentDescending => _commentDescending;
 
   /// Load the user's settings from the SettingsService. It may load from a
   /// local database or the internet. The controller only knows it can load the
   /// settings from the service.
   Future<void> loadSettings() async {
     _themeMode = await _settingsService.themeMode();
-
+    _loginUser = await _settingsService.loginUser();
+    _apiUrl = await _settingsService.apiUrl();
+    _miPushAppId = await _settingsService.miPushAppId();
+    _miPushAppKey = await _settingsService.miPushAppKey();
+    _miPushRegId = await _settingsService.miPushRegId();
+    _refreshInterval = await _settingsService.refreshInterval();
+    _adminUrl = await _settingsService.adminUrl();
+    _blogUrl = await _settingsService.blogUrl();
+    _blogAdminUrl = await _settingsService.blogAdminUrl();
+    _defaultPage = await _settingsService.defaultPage();
+    _commentDescending = await _settingsService.commentDescending();
     // Important! Inform listeners a change has occurred.
     notifyListeners();
   }
@@ -47,5 +81,125 @@ class SettingsController with ChangeNotifier {
     // Persist the changes to a local database or the internet using the
     // SettingService.
     await _settingsService.updateThemeMode(newThemeMode);
+  }
+
+  Future<void> updateLoginUser(User newLoginUser) async {
+    if (newLoginUser == _loginUser) return;
+
+    _loginUser = newLoginUser;
+
+    notifyListeners();
+
+    await _settingsService.updateLoginUser(newLoginUser);
+  }
+
+  Future<void> updateApiUrl(String? newApiUrl) async {
+    if (newApiUrl == null) return;
+
+    if (newApiUrl == _apiUrl) return;
+
+    _apiUrl = newApiUrl;
+
+    notifyListeners();
+
+    await _settingsService.updateApiUrl(newApiUrl);
+  }
+
+  Future<void> updateMiPushAppId(String? newMiPushAppId) async {
+    if (newMiPushAppId == null) return;
+
+    if (newMiPushAppId == _miPushAppId) return;
+
+    _miPushAppId = newMiPushAppId;
+
+    notifyListeners();
+
+    await _settingsService.updateMiPushAppId(newMiPushAppId);
+  }
+
+  Future<void> updateMiPushAppKey(String? newMiPushAppKey) async {
+    if (newMiPushAppKey == null) return;
+
+    if (newMiPushAppKey == _miPushAppKey) return;
+
+    _miPushAppKey = newMiPushAppKey;
+
+    notifyListeners();
+
+    await _settingsService.updateMiPushAppKey(newMiPushAppKey);
+  }
+
+  Future<void> updateMiPushRegId(String? newMiPushRegId) async {
+    if (newMiPushRegId == null) return;
+
+    if (newMiPushRegId == _miPushRegId) return;
+
+    _miPushRegId = newMiPushRegId;
+
+    notifyListeners();
+
+    await _settingsService.updateMiPushRegId(newMiPushRegId);
+  }
+
+  Future<void> updateRefreshInterval(int newRefreshInterval) async {
+    if (newRefreshInterval == _refreshInterval) return;
+
+    _refreshInterval = newRefreshInterval;
+
+    notifyListeners();
+
+    await _settingsService.updateRefreshInterval(newRefreshInterval);
+  }
+
+  Future<void> updateAdminUrl(String? newAdminUrl) async {
+    if (newAdminUrl == null) return;
+
+    if (newAdminUrl == _adminUrl) return;
+
+    _adminUrl = newAdminUrl;
+
+    notifyListeners();
+
+    await _settingsService.updateAdminUrl(newAdminUrl);
+  }
+
+  Future<void> updateBlogUrl(String newBlogUrl) async {
+    if (newBlogUrl == _blogUrl) return;
+
+    _blogUrl = newBlogUrl;
+
+    notifyListeners();
+
+    await _settingsService.updateBlogUrl(newBlogUrl);
+  }
+
+  Future<void> updateBlogAdminUrl(String? newBlogAdminUrl) async {
+    if (newBlogAdminUrl == null) return;
+
+    if (newBlogAdminUrl == _blogAdminUrl) return;
+
+    _blogAdminUrl = newBlogAdminUrl;
+
+    notifyListeners();
+
+    await _settingsService.updateBlogAdminUrl(newBlogAdminUrl);
+  }
+
+  Future<void> updateDefaultPage(AppTab newDefaultPage) async {
+    if (newDefaultPage == _defaultPage) return;
+
+    _defaultPage = newDefaultPage;
+
+    notifyListeners();
+
+    await _settingsService.updateDefaultPage(newDefaultPage);
+  }
+
+  Future<void> updateCommentDescending(bool newCommentDescending) async {
+    if (newCommentDescending == _commentDescending) return;
+
+    _commentDescending = newCommentDescending;
+
+    await _settingsService.updateCommentDescending(newCommentDescending);
   }
 }
--- lib/core/settings/settings_service.dart
+++ lib/core/settings/settings_service.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smarthome/core/core.dart';
+import 'package:smarthome/user/model/user.dart';
 
 /// A service that stores and retrieves user settings.
 ///
@@ -9,27 +14,128 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SettingsService {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  /// Loads the User's preferred ThemeMode from local or remote storage.
   Future<ThemeMode> themeMode() async {
     final prefs = await _prefs;
-    final String themeModeString = prefs.getString('themeMode') ?? 'system';
-    return themeModeString == 'system'
-        ? ThemeMode.system
-        : themeModeString == 'light'
-            ? ThemeMode.light
-            : ThemeMode.dark;
+    final String themeModeString = prefs.getString('themeMode') ?? '';
+    return EnumToString.fromString(ThemeMode.values, themeModeString) ??
+        ThemeMode.system;
   }
 
-  /// Persists the user's preferred ThemeMode to local or remote storage.
   Future<void> updateThemeMode(ThemeMode theme) async {
-    // Use the shared_preferences package to persist settings locally or the
-    // http package to persist settings over the network.
     final prefs = await _prefs;
-    final String themeModeString = theme == ThemeMode.system
-        ? 'system'
-        : theme == ThemeMode.light
-            ? 'light'
-            : 'dark';
-    prefs.setString('themeMode', themeModeString);
+    prefs.setString('themeMode', EnumToString.convertToString(theme));
+  }
+
+  Future<String?> apiUrl() async {
+    final prefs = await _prefs;
+    return prefs.getString('apiUrl');
+  }
+
+  Future<void> updateApiUrl(String url) async {
+    final prefs = await _prefs;
+    prefs.setString('apiUrl', url);
+  }
+
+  Future<String?> miPushAppId() async {
+    final prefs = await _prefs;
+    return prefs.getString('miPushAppId');
+  }
+
+  Future<void> updateMiPushAppId(String appId) async {
+    final prefs = await _prefs;
+    prefs.setString('miPushAppId', appId);
+  }
+
+  Future<String?> miPushAppKey() async {
+    final prefs = await _prefs;
+    return prefs.getString('miPushAppKey');
+  }
+
+  Future<void> updateMiPushAppKey(String appKey) async {
+    final prefs = await _prefs;
+    prefs.setString('miPushAppKey', appKey);
+  }
+
+  Future<String?> miPushRegId() async {
+    final prefs = await _prefs;
+    return prefs.getString('miPushRegId');
+  }
+
+  Future<void> updateMiPushRegId(String regId) async {
+    final prefs = await _prefs;
+    prefs.setString('miPushRegId', regId);
+  }
+
+  Future<int> refreshInterval() async {
+    final prefs = await _prefs;
+    return prefs.getInt('refreshInterval') ?? 10;
+  }
+
+  Future<void> updateRefreshInterval(int interval) async {
+    final prefs = await _prefs;
+    prefs.setInt('refreshInterval', interval);
+  }
+
+  Future<String?> adminUrl() async {
+    final prefs = await _prefs;
+    return prefs.getString('adminUrl');
+  }
+
+  Future<void> updateAdminUrl(String url) async {
+    final prefs = await _prefs;
+    prefs.setString('adminUrl', url);
+  }
+
+  Future<String> blogUrl() async {
+    final prefs = await _prefs;
+    return prefs.getString('blogUrl') ?? 'https://hehome.xyz';
+  }
+
+  Future<void> updateBlogUrl(String url) async {
+    final prefs = await _prefs;
+    prefs.setString('blogUrl', url);
+  }
+
+  Future<String?> blogAdminUrl() async {
+    final prefs = await _prefs;
+    return prefs.getString('blogAdminUrl');
+  }
+
+  Future<void> updateBlogAdminUrl(String url) async {
+    final prefs = await _prefs;
+    prefs.setString('blogAdminUrl', url);
+  }
+
+  Future<AppTab> defaultPage() async {
+    final prefs = await _prefs;
+    final String defaultPageString = prefs.getString('defaultPage') ?? '';
+    return EnumToString.fromString(AppTab.values, defaultPageString) ??
+        AppTab.storage;
+  }
+
+  Future<void> updateDefaultPage(AppTab page) async {
+    final prefs = await _prefs;
+    prefs.setString('defaultPage', EnumToString.convertToString(page));
+  }
+
+  Future<User> loginUser() async {
+    final prefs = await _prefs;
+    final String userString = prefs.getString('loginUser') ?? '';
+    return User.fromJson(jsonDecode(userString));
+  }
+
+  Future<void> updateLoginUser(User user) async {
+    final prefs = await _prefs;
+    prefs.setString('loginUser', jsonEncode(user.toJson()));
+  }
+
+  Future<bool> commentDescending() async {
+    final prefs = await _prefs;
+    return prefs.getBool('commentDescending') ?? false;
+  }
+
+  Future<void> updateCommentDescending(bool descending) async {
+    final prefs = await _prefs;
+    prefs.setBool('commentDescending', descending);
   }
 }
