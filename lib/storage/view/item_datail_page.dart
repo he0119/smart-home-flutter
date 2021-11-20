42825e8b5b73b44c8b724657f1fea394ba95ef3b
hemengyang <hmy0119@gmail.com>
2021-11-20 21:48:08 +0800

    优化物品详情界面的体验

--- lib/storage/view/item_datail_page.dart
+++ lib/storage/view/item_datail_page.dart
@@ -59,20 +59,13 @@ class ItemDetailPage extends Page {
   }
 }
 
-class ItemDetailScreen extends StatefulWidget {
+class ItemDetailScreen extends StatelessWidget {
   final String itemId;
 
   const ItemDetailScreen({
     Key? key,
     required this.itemId,
   }) : super(key: key);
-
-  @override
-  _ItemDetailScreenState createState() => _ItemDetailScreenState();
-}
-
-class _ItemDetailScreenState extends State<ItemDetailScreen> {
-  String itemName = '';
 
   @override
   Widget build(BuildContext context) {
@@ -105,123 +98,117 @@ class _ItemDetailScreenState extends State<ItemDetailScreen> {
   }
 
   AppBar _buildAppBar(BuildContext context, ItemDetailState state) {
-    if (state is ItemDetailSuccess) {
-      itemName = state.item.name;
-      return AppBar(
-        title: Text(itemName),
-        actions: <Widget>[
-          const SearchIconButton(),
-          PopupMenuButton<ItemDetailMenu>(
-            onSelected: (value) async {
-              if (value == ItemDetailMenu.edit) {
-                await Navigator.of(context).push(MaterialPageRoute(
-                  builder: (_) => BlocProvider<ItemEditBloc>(
-                    create: (_) => ItemEditBloc(
-                      storageRepository: context.read<StorageRepository>(),
-                    ),
-                    child: ItemEditPage(
-                      isEditing: true,
-                      item: state.item,
-                    ),
-                  ),
-                ));
-                context.read<ItemDetailBloc>().add(
-                      ItemDetailStarted(
-                        id: state.item.id,
-                      ),
-                    );
-              }
-              if (value == ItemDetailMenu.consumable) {
-                await Navigator.of(context).push(MaterialPageRoute(
-                  builder: (_) => BlocProvider<ItemEditBloc>(
-                    create: (_) => ItemEditBloc(
-                      storageRepository: context.read<StorageRepository>(),
-                    ),
-                    child: ConsumableEditPage(
-                      item: state.item,
-                    ),
-                  ),
-                ));
-                context.read<ItemDetailBloc>().add(
-                      ItemDetailStarted(
-                        id: state.item.id,
-                      ),
-                    );
-              }
-              if (value == ItemDetailMenu.addPicture) {
-                if (kIsWeb) {
-                  showInfoSnackBar('抱歉，暂时不支持通过网页添加图片');
-                } else {
-                  MyRouterDelegate.of(context)
-                      .push(PictureAddPage(itemId: state.item.id));
-                }
-              }
-              if (value == ItemDetailMenu.delete) {
-                await showDialog(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                    title: Text('删除 ${state.item.name}'),
-                    content: const Text('你确认要删除该物品么？'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('否'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          context.read<ItemEditBloc>().add(
-                                ItemDeleted(item: state.item),
-                              );
-                          Navigator.pop(context);
-                        },
-                        child: const Text('是'),
-                      ),
-                    ],
-                  ),
-                );
-              }
-            },
-            itemBuilder: (context) => [
-              const PopupMenuItem(
-                value: ItemDetailMenu.edit,
-                child: Text('编辑'),
-              ),
-              const PopupMenuItem(
-                value: ItemDetailMenu.addPicture,
-                child: Text('添加图片'),
-              ),
-              const PopupMenuItem(
-                value: ItemDetailMenu.consumable,
-                child: Text('耗材编辑'),
-              ),
-              const PopupMenuItem(
-                value: ItemDetailMenu.delete,
-                child: Text('删除'),
-              ),
-            ],
-          )
-        ],
-      );
-    }
     return AppBar(
-      title: Text(itemName),
+      title: Text(state.item.name),
+      actions: <Widget>[
+        const SearchIconButton(),
+        PopupMenuButton<ItemDetailMenu>(
+          onSelected: (value) async {
+            if (value == ItemDetailMenu.edit) {
+              await Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => BlocProvider<ItemEditBloc>(
+                  create: (_) => ItemEditBloc(
+                    storageRepository: context.read<StorageRepository>(),
+                  ),
+                  child: ItemEditPage(
+                    isEditing: true,
+                    item: state.item,
+                  ),
+                ),
+              ));
+              context.read<ItemDetailBloc>().add(
+                    ItemDetailStarted(
+                      id: state.item.id,
+                    ),
+                  );
+            }
+            if (value == ItemDetailMenu.consumable) {
+              await Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => BlocProvider<ItemEditBloc>(
+                  create: (_) => ItemEditBloc(
+                    storageRepository: context.read<StorageRepository>(),
+                  ),
+                  child: ConsumableEditPage(
+                    item: state.item,
+                  ),
+                ),
+              ));
+              context.read<ItemDetailBloc>().add(
+                    ItemDetailStarted(
+                      id: state.item.id,
+                    ),
+                  );
+            }
+            if (value == ItemDetailMenu.addPicture) {
+              if (kIsWeb) {
+                showInfoSnackBar('抱歉，暂时不支持通过网页添加图片');
+              } else {
+                MyRouterDelegate.of(context)
+                    .push(PictureAddPage(itemId: state.item.id));
+              }
+            }
+            if (value == ItemDetailMenu.delete) {
+              await showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: Text('删除 ${state.item.name}'),
+                  content: const Text('你确认要删除该物品么？'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('否'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        context.read<ItemEditBloc>().add(
+                              ItemDeleted(item: state.item),
+                            );
+                        Navigator.pop(context);
+                      },
+                      child: const Text('是'),
+                    ),
+                  ],
+                ),
+              );
+            }
+          },
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              value: ItemDetailMenu.edit,
+              child: Text('编辑'),
+            ),
+            const PopupMenuItem(
+              value: ItemDetailMenu.addPicture,
+              child: Text('添加图片'),
+            ),
+            const PopupMenuItem(
+              value: ItemDetailMenu.consumable,
+              child: Text('耗材编辑'),
+            ),
+            const PopupMenuItem(
+              value: ItemDetailMenu.delete,
+              child: Text('删除'),
+            ),
+          ],
+        )
+      ],
     );
   }
 
   Widget _buildBody(BuildContext context, ItemDetailState state) {
-    if (state is ItemDetailFailure) {
+    if (state.status == ItemDetailStatus.failure) {
       return ErrorMessageButton(
         onPressed: () {
           context.read<ItemDetailBloc>().add(
-                ItemDetailStarted(id: state.id),
+                ItemDetailStarted(id: itemId),
               );
         },
-        message: state.message,
+        message: state.error,
       );
     }
-    if (state is ItemDetailSuccess) {
+    if (state.status == ItemDetailStatus.success) {
       return _ItemDetailList(item: state.item);
     }
     return const CenterLoadingIndicator();
