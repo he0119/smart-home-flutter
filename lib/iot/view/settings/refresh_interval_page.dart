c15f95227e624bc341b8c24af015216bba783b47
hemengyang <hmy0119@gmail.com>
2021-12-12 16:06:08 +0800

    style: 删除多余的 import

--- lib/iot/view/settings/refresh_interval_page.dart
+++ lib/iot/view/settings/refresh_interval_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:smarthome/app/settings/settings_controller.dart';
 import 'package:smarthome/widgets/text_edit_page.dart';
