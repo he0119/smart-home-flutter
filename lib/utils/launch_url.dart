4ee8698e2f93598d4369c74174314fb7f16785c5
hemengyang <hmy0119@gmail.com>
2022-05-12 10:28:46 +0800

    修复 url_launcher 的弃用警告

--- lib/utils/launch_url.dart
+++ lib/utils/launch_url.dart
@@ -2,8 +2,8 @@ import 'package:url_launcher/url_launcher.dart';
 
 /// 打开网址
 Future launchUrl(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
+  if (await canLaunchUrl(Uri.parse(url))) {
+    await launchUrl(url);
   } else {
     throw Exception('Could not launch $url');
   }
