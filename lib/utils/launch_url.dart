b735b8061758d5d076ce0cad66ba9edb5a845537
hemengyang <hmy0119@gmail.com>
2022-06-05 19:42:49 +0800

    fix: 修复打开网址卡住的问题 (#211)
    没想到是因为名字相同导致递归

--- lib/utils/launch_url.dart
+++ lib/utils/launch_url.dart
@@ -1,9 +1,10 @@
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
 /// 打开网址
-Future launchUrl(String url) async {
-  if (await canLaunchUrl(Uri.parse(url))) {
-    await launchUrl(url);
+Future<void> launchUrl(String url) async {
+  final uri = Uri.parse(url);
+  if (await url_launcher.canLaunchUrl(uri)) {
+    await url_launcher.launchUrl(uri);
   } else {
     throw Exception('Could not launch $url');
   }
