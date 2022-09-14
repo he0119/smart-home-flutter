70bf3d251631e72e49fefa2133916f7396dc4c5f
hemengyang <hmy0119@gmail.com>
2022-09-14 12:32:11 +0800

    change: 改回 UserAccountsDrawerHeader 默认颜色
    之前设置的颜色在网页版看起来太淡了，比较奇怪，还是改回来吧

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -25,19 +25,11 @@ class MyDrawer extends StatelessWidget {
         children: [
           if (loginUser != null)
             UserAccountsDrawerHeader(
-              accountName: Text(
-                loginUser.username,
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.onBackground),
-              ),
-              accountEmail: Text(loginUser.email!,
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.onBackground)),
+              accountName: Text(loginUser.username),
+              accountEmail: Text(loginUser.email!),
               currentAccountPicture: MyCircleAvatar(
                 avatarUrl: loginUser.avatarUrl,
               ),
-              decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.background),
             ),
           ListTile(
             title: const Text('耗材管理'),
