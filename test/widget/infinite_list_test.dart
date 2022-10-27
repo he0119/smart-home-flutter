309b137cc64de2cc9ea1e858a0a873d0631823cf
hemengyang <hmy0119@gmail.com>
2022-10-27 22:35:21 +0800

    如果加载完整则不用显示底部加载指示

--- test/widget/infinite_list_test.dart
+++ test/widget/infinite_list_test.dart
@@ -11,9 +11,57 @@ abstract class OnFetch {
 class MockOnFetch extends Mock implements OnFetch {}
 
 void main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows you to build and interact
-  // with widgets in the test environment.
+  testWidgets(
+    'InfiniteList should not show loading indicator when reached max',
+    (tester) async {
+      final List<int> items = List<int>.generate(20, (int i) => i);
+      const double itemHeight = 300.0;
+      const double viewportHeight = 500.0;
+      const double scrollPosition = 19 * itemHeight;
+      final ScrollController controller = ScrollController();
+      final mockOnFetch = MockOnFetch();
+
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: SizedBox(
+            height: viewportHeight,
+            child: CustomScrollView(
+              controller: controller,
+              slivers: [
+                SliverInfiniteList<int>(
+                  items: items,
+                  itemBuilder: (context, item) {
+                    return SizedBox(
+                      height: itemHeight,
+                      child: Text('Tile $item'),
+                    );
+                  },
+                  hasReachedMax: true,
+                  onFetch: mockOnFetch,
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      expect(controller.offset, 0);
+      expect(find.text('Tile 0'), findsOneWidget);
+      expect(find.text('Tile 19'), findsNothing);
+      expect(find.byType(BottomLoader), findsNothing);
+      verifyNever(() => mockOnFetch());
+
+      controller.jumpTo(scrollPosition);
+      await tester.pump();
+
+      expect(find.text('Tile 0'), findsNothing);
+      expect(find.text('Tile 19'), findsOneWidget);
+      expect(find.byType(BottomLoader), findsNothing);
+      verifyNever(() => mockOnFetch());
+    },
+  );
+
   testWidgets(
     'InfiniteList should show loading indicator when loading',
     (tester) async {
