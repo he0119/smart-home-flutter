ecfb71d7e2c1fd85e43f20411cfd0a4339290b9f
uy/sun <hmy0119@gmail.com>
2025-02-19 20:17:27 +0800

    fix: 修复 Zone mismatch 的问题

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -14,26 +14,6 @@ import 'package:smarthome/core/model/app_config.dart';
 import 'package:smarthome/core/repository/graphql_api_client.dart';
 
 Future<void> bootstrap(AppConfig appConfig) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // 设置导航栏透明
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    systemNavigationBarColor: Colors.transparent,
-  ));
-  // 根据是否是网页环境，初始化不同的配置
-  await configureApp();
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService(), appConfig);
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-  // 初始化 GraphQL API Client
-  final graphQLApiClient = GraphQLApiClient(settingsController);
-  await graphQLApiClient.loadSettings();
-  // 配置 BLoC 的日志
-  Bloc.observer = SimpleBlocObserver();
-
   await SentryFlutter.init(
     (options) {
       options
@@ -41,11 +21,33 @@ Future<void> bootstrap(AppConfig appConfig) async {
         ..environment = appConfig.flavorName
         ..release = const String.fromEnvironment('SENTRY_RELEASE');
     },
-    appRunner: () => runApp(
-      MyApp(
-        settingsController: settingsController,
-        graphQLApiClient: graphQLApiClient,
-      ),
-    ),
+    appRunner: () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      // 设置导航栏透明
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        systemNavigationBarColor: Colors.transparent,
+      ));
+      // 根据是否是网页环境，初始化不同的配置
+      await configureApp();
+      // Set up the SettingsController, which will glue user settings to multiple
+      // Flutter Widgets.
+      final settingsController =
+          SettingsController(SettingsService(), appConfig);
+      // Load the user's preferred theme while the splash screen is displayed.
+      // This prevents a sudden theme change when the app is first displayed.
+      await settingsController.loadSettings();
+      // 初始化 GraphQL API Client
+      final graphQLApiClient = GraphQLApiClient(settingsController);
+      await graphQLApiClient.loadSettings();
+      // 配置 BLoC 的日志
+      Bloc.observer = SimpleBlocObserver();
+      runApp(
+        MyApp(
+          settingsController: settingsController,
+          graphQLApiClient: graphQLApiClient,
+        ),
+      );
+    },
   );
 }
