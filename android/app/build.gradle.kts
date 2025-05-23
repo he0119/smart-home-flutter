f866b0011670c5b51c51d1434548df6a7f1b21f3
uy/sun <hmy0119@gmail.com>
2025-05-23 13:16:21 +0800

    fix: 更新 ndkVersion 为 27.0.12077973
    Your project is configured with Android NDK 26.3.11579264, but the following plugin(s) depend on a different Android NDK version:
    - android_id requires Android NDK 27.0.12077973
    - device_info_plus requires Android NDK 27.0.12077973
    - dynamic_color requires Android NDK 27.0.12077973
    - flutter_keyboard_visibility requires Android NDK 27.0.12077973
    - flutter_plugin_android_lifecycle requires Android NDK 27.0.12077973
    - image_picker_android requires Android NDK 27.0.12077973
    - package_info_plus requires Android NDK 27.0.12077973
    - path_provider_android requires Android NDK 27.0.12077973
    - quick_actions_android requires Android NDK 27.0.12077973
    - sentry_flutter requires Android NDK 27.0.12077973
    - share_plus requires Android NDK 27.0.12077973
    - shared_preferences_android requires Android NDK 27.0.12077973
    - sqflite_android requires Android NDK 27.0.12077973
    - url_launcher_android requires Android NDK 27.0.12077973
    - webview_flutter_android requires Android NDK 27.0.12077973
    Fix this issue by using the highest Android NDK version (they are backward compatible).
    Add the following to C:\Users\hmy01\Works\Working\SmartHome\smart-home-flutter\android\app\build.gradle.kts:
    
        android {
            ndkVersion = "27.0.12077973"
            ...
        }

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "xyz.hehome.smarthome"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
