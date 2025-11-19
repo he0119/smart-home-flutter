3f67cca9b14e88dab99f22bd6d71fb02a44d326c
uy/sun <hmy0119@gmail.com>
2025-11-19 09:56:28 +0800

    refactor: 调整路径常量顺序

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -3,19 +3,35 @@ import 'package:go_router/go_router.dart';
 
 // 路由路径常量
 class AppRoutes {
-  static const String login = '/login';
   static const String home = '/';
+  static const String login = '/login';
+
+  // 物品管理
   static const String storage = '/storage';
   static const String storageDetail = '/storage/:id';
   static const String itemDetail = '/item/:id';
-  static const String topicDetail = '/topic/:id';
+  static const String itemEdit = '/item/:id/edit';
   static const String pictureDetail = '/picture/:id';
+  static const String itemPictureAdd = '/item/:id/picture/add';
+  static const String search = '/search';
   static const String consumables = '/consumables';
   static const String recycleBin = '/recyclebin';
+
+  // Special token for the virtual root storage detail page
+  static const String storageRootId = 'root';
+  static const String storageRootDetail = '$storage/$storageRootId';
+
+  // 留言板
+  static const String board = '/board';
+  static const String topicDetail = '/topic/:id';
+
+  // 博客
+  static const String blog = '/blog';
+
+  // 设置
+  static const String admin = '/admin';
   static const String settings = '/settings';
   static const String blogSettings = '/settings/blog';
-
-  // 设置子页面
   static const String themeSettings = '/settings/theme';
   static const String apiUrlSettings = '/settings/api-url';
   static const String adminUrlSettings = '/settings/admin-url';
@@ -25,17 +41,6 @@ class AppRoutes {
   static const String blogUrlSettings = '/settings/blog-url';
   static const String blogAdminUrlSettings = '/settings/blog-admin-url';
   static const String commentOrderSettings = '/settings/comment-order';
-
-  static const String blog = '/blog';
-  static const String board = '/board';
-  static const String search = '/search';
-  static const String admin = '/admin';
-  static const String itemEdit = '/item/:id/edit';
-  static const String itemPictureAdd = '/item/:id/picture/add';
-
-  // Special token for the virtual root storage detail page
-  static const String storageRootId = 'root';
-  static const String storageRootDetail = '$storage/$storageRootId';
 
   // Build a concrete storage detail path, mapping the root id when needed
   static String storageDetailPath(String storageId) {
