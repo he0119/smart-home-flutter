76ffa7165100bbe3b03ce9e90995415782813cff
uy/sun <hmy0119@gmail.com>
2026-05-21 13:57:22 +0800

    feat(ui): 添加宽屏导航栏

--- lib/widgets/tab_selector.dart
+++ lib/widgets/tab_selector.dart
@@ -4,11 +4,13 @@ import 'package:smarthome/core/core.dart';
 class TabSelector extends StatelessWidget {
   final AppTab activeTab;
   final Function(AppTab) onTabSelected;
+  final bool extended;
 
   const TabSelector({
     super.key,
     required this.activeTab,
     required this.onTabSelected,
+    this.extended = false,
   });
 
   @override
@@ -16,6 +18,39 @@ class TabSelector extends StatelessWidget {
     return NavigationBar(
       destinations: AppTab.values
           .map((tab) => NavigationDestination(icon: tab.icon, label: tab.name))
+          .toList(),
+      selectedIndex: AppTab.values.indexOf(activeTab),
+      onDestinationSelected: (index) => onTabSelected(AppTab.values[index]),
+    );
+  }
+}
+
+class RailTabSelector extends StatelessWidget {
+  final AppTab activeTab;
+  final Function(AppTab) onTabSelected;
+  final bool extended;
+
+  const RailTabSelector({
+    super.key,
+    required this.activeTab,
+    required this.onTabSelected,
+    this.extended = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      extended: extended,
+      labelType: extended
+          ? NavigationRailLabelType.none
+          : NavigationRailLabelType.selected,
+      destinations: AppTab.values
+          .map(
+            (tab) => NavigationRailDestination(
+              icon: tab.icon,
+              label: Text(tab.name),
+            ),
+          )
           .toList(),
       selectedIndex: AppTab.values.indexOf(activeTab),
       onDestinationSelected: (index) => onTabSelected(AppTab.values[index]),
